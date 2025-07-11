be8bc8397689541e26f5489e6a64d05dab1b0b69
Ayushmaan1786 <96243937+Ayushmaan1786@users.noreply.github.com>
2025-07-11 20:27:18 +0530

    create_estimate is working

--- rctiles/admin_dashboard/admin_header.php
+++ rctiles/admin_dashboard/admin_header.php
@@ -1,13 +1,13 @@
 <?php
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
+// if (session_status() == PHP_SESSION_NONE) {
+//     session_start();
+// }
 
 
-if(!isset($_SESSION['user_id']) || $_SESSION['role_id'] != 1) {
-    header("Location: ../login.php");
-    exit;
-}
+// if(!isset($_SESSION['user_id']) || $_SESSION['role_id'] != 1) {
+//     header("Location: ../login.php");
+//     exit;
+// }
 
 
 
